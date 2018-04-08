34df92fe7febc31ca5f7d1ad6bf32f19901c1759
D3ath22 <pedro.monteiro22@gmail.com>
2018-04-08 13:11:48 +0100

    Mazerunner execution

--- src/ServeRequest.java
+++ src/ServeRequest.java
@@ -6,6 +6,12 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import pt.ulisboa.tecnico.meic.cnv.mazerunner.maze.Main;
+import pt.ulisboa.tecnico.meic.cnv.mazerunner.maze.exceptions.CantGenerateOutputFileException;
+import pt.ulisboa.tecnico.meic.cnv.mazerunner.maze.exceptions.CantReadMazeInputFileException;
+import pt.ulisboa.tecnico.meic.cnv.mazerunner.maze.exceptions.InvalidCoordinatesException;
+import pt.ulisboa.tecnico.meic.cnv.mazerunner.maze.exceptions.InvalidMazeRunningStrategyException;
+
 class ServeRequest implements Runnable {
    private Thread t;
    private String name;
@@ -19,6 +25,18 @@ class ServeRequest implements Runnable {
    
    public void run() {
       try {
+    	  
+    	 //run requested mazerunner
+    	 Main m = new Main();
+    	 String[] args = {"3", "9", "78", "89", "50", "astar", "MazeRunner/Maze100.maze", "Maze100.html"};
+    	 try {
+			m.main(args);
+		} catch (InvalidMazeRunningStrategyException | InvalidCoordinatesException | CantGenerateOutputFileException
+				| CantReadMazeInputFileException e) {
+			e.printStackTrace();
+		}
+    	 
+    	 //send html response
          System.out.println(name + " going to sleep");
          Thread.sleep(5000);
          System.out.println(name + " awake");
